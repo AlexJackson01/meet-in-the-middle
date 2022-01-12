56f1151d697d181da5759d88a78d016e60a4a010
AlexJackson01 <ajjackson22@gmail.com>
2022-01-12 18:44:25 +0000

    Sorted results by rating and limited to top 5, display star images based on rating

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,27 @@ import 'leaflet/dist/leaflet.css';
 import axios from 'axios';
 // import LogoNav from "./components/LogoNav";
 import Map from "./components/Map";
-import NearbySearch from "./components/NearbySearch"; 
+import NearbySearch from "./components/NearbySearch";
+import Half from './images/star-ratings/0.5-star.png';
+import One from './images/star-ratings/1-star.png';
+import OneHalf from './images/star-ratings/1.5-star.png';
+import Two from './images/star-ratings/2-stars.png';
+import TwoHalf from './images/star-ratings/2.5-star.png';
+import Three from './images/star-ratings/3-stars.png';
+import ThreeHalf from './images/star-ratings/3.5-star.png';
+import Four from './images/star-ratings/4-stars.png';
+import FourHalf from './images/star-ratings/4.5-star.png';
+import Five from './images/star-ratings/5-stars.png';
+import FiveHalf from './images/star-ratings/5.5-stars.png';
+import Six from './images/star-ratings/6-stars.png';
+import SixHalf from './images/star-ratings/6.5-stars.png';
+import Seven from './images/star-ratings/7-stars.png';
+import SevenHalf from './images/star-ratings/7.5-stars.png';
+import Eight from './images/star-ratings/8-stars.png';
+import EightHalf from './images/star-ratings/8.5-stars.png';
+import Nine from './images/star-ratings/9-stars.png';
+import NineHalf from './images/star-ratings/9.5-stars.png';
+import Ten from './images/star-ratings/10-stars.png'; 
 
 function App() {
   let [loading, setLoading] = useState(false);
@@ -103,25 +123,85 @@ function App() {
       const searchOne = details.filter(place => place.dataSources !== undefined);
 
       for (let place of searchOne) {
-      nearbyDetails.push({
-        id: place.dataSources.poiDetails[0].id,
-        name: place.poi.name,
-        address: place.address.freeformAddress,
-        url: place.poi.url
-      })
+        nearbyDetails.push({
+          id: place.dataSources.poiDetails[0].id,
+          name: place.poi.name,
+          address: place.address.freeformAddress,
+          url: place.poi.url
+        })
       }
       console.log(nearbyDetails);
 
       for (let place of searchOne) {
         const res2 = await axios.get(`https://api.tomtom.com/search/2/poiDetails.json?key=${nearbyKey}&id=${place.dataSources.poiDetails[0].id}`);
-          extendedID.push({
-            id: res2.data.id,
-            rating: res2.data.result.rating
-          })
+        extendedID.push({
+          id: res2.data.id,
+          rating: res2.data.result.rating,
+          reviews: res2.data.result.reviews,
+          stars: ""
+        })
       }
 
       let fullDetails = nearbyDetails.map((item, i) => Object.assign({}, item, extendedID[i]));
-      setNearby(fullDetails);
+      console.log(fullDetails);
+      // setNearby(fullDetails);
+
+      const searchTwo = fullDetails.filter(place => place.rating !== undefined);
+
+      let sorted = searchTwo.sort((a, b) => (b.rating.value) - (a.rating.value));
+      let top5 = sorted.slice(0, 5);
+
+      top5.forEach(place => {
+        if (place.rating >= 0 && place.rating <= 0.7) {
+          place.stars = Half;
+        } else if (place.rating > 0.8 && place.rating <= 1.2) {
+          place.stars = One;
+        } else if (place.rating > 1.2 && place.rating <= 1.7) {
+          place.stars = OneHalf;
+        } else if (place.rating > 1.7 && place.rating <= 2.2) {
+          place.stars = Two;
+        } else if (place.rating > 2.2 && place.rating <= 2.7) {
+          place.stars = TwoHalf;
+        } else if (place.rating > 2.7 && place.rating <= 3.2) {
+          place.stars = Three;
+        } else if (place.rating > 3.2 && place.rating <= 3.7) {
+          place.stars = ThreeHalf;
+        } else if (place.rating > 3.7 && place.rating <= 4.2) {
+          place.stars = Four;
+        } else if (place.rating > 4.2 && place.rating <= 4.7) {
+          place.stars = FourHalf;
+        } else if (place.rating > 4.7 && place.rating <= 5) {
+          place.stars = Five;
+        } else if (place.rating.value > 5 && place.rating.value <= 5.7) {
+            place.stars = FiveHalf;
+        } else if (place.rating.value > 5.7 && place.rating.value <= 6.2) {
+            place.stars = Six;
+        } else if (place.rating.value > 6.2 && place.rating.value <= 6.7) {
+            place.stars = SixHalf;
+        } else if (place.rating.value > 6.7 && place.rating.value <= 7.2) {
+            place.stars = Seven;
+        } else if (place.rating.value > 7.2 && place.rating.value <= 7.7) {
+            place.stars = SevenHalf;
+        } else if (place.rating.value > 7.7 && place.rating.value <= 8.2) {
+            place.stars = Eight;
+        } else if (place.rating.value > 8.2 && place.rating.value <= 8.7) {
+            place.stars = EightHalf;
+        } else if (place.rating.value > 8.7 && place.rating.value <= 9.2) {
+            place.stars = Nine;
+        } else if (place.rating.value > 9.2 && place.rating.value <= 9.7) {
+            place.stars = NineHalf;
+        } else if (place.rating.value > 9.7 && place.rating.value <= 10) {
+            place.stars = Ten;
+        } else {
+            place.stars = "";
+        }
+            
+        }
+      )
+      
+      console.log(top5);
+
+      setNearby(top5);
       
       console.log("i'm here now");
     }
@@ -178,8 +258,8 @@ function App() {
       <button onClick={(e) => clearSearch(e)}>Clear</button>
       
 
-              {loading && <p>Finding...</p>}
-      {nearby && <h5>The midpoint between {input.inputOne.toUpperCase()} and {input.inputTwo.toUpperCase()}:</h5>}
+      {loading && <p>Finding...</p>}
+      {midpoint.lat && <h5>The midpoint between {input.inputOne.toUpperCase()} and {input.inputTwo.toUpperCase()}:</h5>}
       {/* {points.type && <h5>The {points.type} at your midpoint:</h5>} */}
       <Map midpoint={midpoint} />
       {errorMsg}
--- client/src/components/NearbySearch.js
+++ client/src/components/NearbySearch.js
@@ -1,98 +1,29 @@
-import React, {useState, useEffect} from 'react';
-// import Half from '../images/star-ratings/0.5-star.png';
-// import One from '../images/star-ratings/1-star.png';
-// import OneHalf from '../images/star-ratings/1.5-star.png';
-// import Two from '../images/star-ratings/2-stars.png';
-// import TwoHalf from '../images/star-ratings/2.5-star.png';
-// import Three from '../images/star-ratings/3-stars.png';
-// import ThreeHalf from '../images/star-ratings/3.5-star.png';
-// import Four from '../images/star-ratings/4-stars.png';
-// import FourHalf from '../images/star-ratings/4.5-star.png';
-// import Five from '../images/star-ratings/5-stars.png';
+import React, { useEffect } from 'react';
 
 export default function NearbySearch({ nearby }) {
 
-    let [fullDetails, setFullDetails] = useState();
-
-        useEffect(() => {
+    useEffect(() => {
         renderNearby()
     }, [nearby])
 
-    let nearbyKey = "MpOfnHZhRSBv9wbqjqhYWvYAMWkaFeup";
-
-    // if (nearby) {
-    //     setFullDetails(nearby);
-    // }
-
     const renderNearby = () => {
         console.log(nearby);
-        return nearby.length > 1 ? nearby.map((place) => (
-            <div key={place.id}>
-                <h4>{place.name}</h4>
-                <h6>{place.address}</h6>
-                <p>{place.url}</p>
-                {place.rating ? <p>{place.rating.value}</p> : <p>"Rating not available"</p>}
-                {/* <p>{place.rating.rating.value}</p> */}
-            </div>
-        ))
-            : null;
-
+            return nearby.length > 1 ? nearby.map((place) => (
+                <div className="places-list" key={place.id}>
+                    <ul className='place-card'>
+                        <li><h4>{place.name}</h4></li>
+                        <li className='place-url'><a href={place.url}>Visit their website</a></li>
+                        <li><img src={place.stars} className='star-rating' alt="" /></li>
+                        <li>{place.rating ? <p><b>{place.rating.value}</b> (Foursquare)</p> : <p><em>{"Rating not available"}</em></p>}</li>
+                        <li> {place.reviews ? <p><em>Others have said: {place.reviews[0].text}</em></p> : <p><em>{"Reviews not available"}</em></p>}</li>
+                    </ul>
+                </div>
+            ))
+            : "No results found..."
     }
 
-        // console.log(ids);
-//     const getDetails = () => {
-//         for (let id of ids) {
-//             fetch(`https://api.tomtom.com/search/2/poiDetails.json?key=${nearbyKey}&id=${id}`, {
-//                 "method": "GET"
-//             })
-//                 .then(response => { return response.json() })
-//                 .then(response => {
-//                     extended.push(response);
-// //                     // setExtended(response);
-//                 })
-//                 .then(response =>
-//                     setRatings(extended)
-//                 )
-//                 .catch(err => console.log(err))
-//         }
-//     }
 
-    // if (extended) {
-    //     setRatings(extended);
-    // }
-
-
-
-
-
-//    console.log(extended);
-
-    // nearby.forEach(place => {
-    //     if (place.rating >= 0 && place.rating <= 0.7) {
-    //         place.stars = Half;
-    //     } else if (place.rating > 0.8 && place.rating <= 1.2) {
-    //         place.stars = One;
-    //     } else if (place.rating >= 1.3 && place.rating <= 1.7) {
-    //         place.stars = OneHalf;
-    //     } else if (place.rating >= 1.8 && place.rating <= 2.2) {
-    //         place.stars = Two;
-    //     } else if (place.rating >= 2.3 && place.rating <= 2.7) {
-    //         place.stars = TwoHalf;
-    //     } else if (place.rating >= 2.8 && place.rating <= 3.2) {
-    //         place.stars = Three;
-    //     } else if (place.rating >= 3.3 && place.rating <= 3.7) {
-    //         place.stars = ThreeHalf;
-    //     } else if (place.rating >= 3.8 && place.rating <= 4.2) {
-    //         place.stars = Four;
-    //     } else if (place.rating >= 4.3 && place.rating <= 4.7) {
-    //         place.stars = FourHalf;
-    //     } else if (place.rating >= 4.8 && place.rating <= 5) {
-    //         place.stars = Five;
-    //     } else {
-    //         place.stars = "";
-    //     }
-    // })
-
+ 
 
     return (
         <div className='container'>
@@ -100,18 +31,7 @@ export default function NearbySearch({ nearby }) {
 
                 {renderNearby()}
 
-                {/* {fullDetails !== undefined && (fullDetails.map((place) => {
-                    <div key={place.id}>
-                        <h4>{place.name}</h4>
-                        <h6>{place.address}</h6>
-                        <p>{place.url}</p>
-                        <p>Show ratings</p>
-                    </div>
-                }
-                )
-                )} */}
 
-                        {/* <img src={place.stars} className='star-rating' alt="" /> */}
                           {/* <b>{place.rating}</b> */}
             </div>
             </div>
