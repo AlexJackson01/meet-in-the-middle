32740acf99cd98e20080660f0354c587bcff8a33
AlexJackson01 <ajjackson22@gmail.com>
2022-01-13 16:25:20 +0000

    Added image data file in components folder

---
+++ client/src/components/Images/star-images.js
@@ -0,0 +1,44 @@
+import Half from '../../images/star-ratings/0.5-star.png';
+import One from '../../images/star-ratings/1-star.png';
+import OneHalf from '../../images/star-ratings/1.5-star.png';
+import Two from '../../images/star-ratings/2-stars.png';
+import TwoHalf from '../../images/star-ratings/2.5-star.png';
+import Three from '../../images/star-ratings/3-stars.png';
+import ThreeHalf from '../../images/star-ratings/3.5-star.png';
+import Four from '../../images/star-ratings/4-stars.png';
+import FourHalf from '../../images/star-ratings/4.5-star.png';
+import Five from '../../images/star-ratings/5-stars.png';
+import FiveHalf from '../../images/star-ratings/5.5-stars.png';
+import Six from '../../images/star-ratings/6-stars.png';
+import SixHalf from '../../images/star-ratings/6.5-stars.png';
+import Seven from '../../images/star-ratings/7-stars.png';
+import SevenHalf from '../../images/star-ratings/7.5-stars.png';
+import Eight from '../../images/star-ratings/8-stars.png';
+import EightHalf from '../../images/star-ratings/8.5-stars.png';
+import Nine from '../../images/star-ratings/9-stars.png';
+import NineHalf from '../../images/star-ratings/9.5-stars.png';
+import Ten from '../../images/star-ratings/10-stars.png';
+
+export const image_data = [
+    { image_name: "Half", image: Half },
+    { image_name: "One", image: One },
+    { image_name: "OneHalf", image: OneHalf },
+    { image_name: "Two", image: Two },
+    { image_name: "TwoHalf", image: TwoHalf },
+    { image_name: "Three", image: Three },
+    { image_name: "ThreeHalf", image: ThreeHalf },
+    { image_name: "Four", image: Four },
+    { image_name: "FourHalf", image: FourHalf },
+    { image_name: "Five", image: Five },
+    { image_name: "FiveHalf", image: FiveHalf },
+    { image_name: "Six", image: Six },
+    { image_name: "SixHalf", image: SixHalf },
+    { image_name: "Seven", image: Seven },
+    { image_name: "SevenHalf", image: SevenHalf },
+    { image_name: "Eight", image: Eight },
+    { image_name: "EightHalf", image: EightHalf },
+    { image_name: "Nine", image: Nine },
+    { image_name: "NineHalf", image: NineHalf },
+    { image_name: "Ten", image: Ten }
+
+];
